f4f93be6c3eef326086f93aaa7b44536d240ca6f
Michael Wood <michael.g.wood@intel.com>
2015-01-27 17:23:33 +0000

    Test: Add test script to send an error to a live error-repor-web server
    Usage: test-send-error.py url json_data_file
    
    Signed-off-by: Michael Wood <michael.g.wood@intel.com>

---
+++ test-data/test-send-error.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# test/example script for sending data to error-report-web
+
+import urllib2
+import sys
+import urllib
+
+def send_data (url, data_file):
+        print "===== Sending ===="
+        print data_file + " to " + url
+
+        with open(data_file) as f:
+            data = f.read()
+
+        data = urllib.urlencode({'data': data })
+
+        req = urllib2.Request(url, data=data, headers={'Content-type': 'application/json'})
+        response = urllib2.urlopen(req)
+
+        print "===== Response ===="
+        print response.read()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print ("Please specify a url and data file\nUsage:\n\t test-send-error.py <url> <json data file path> \nExample:\n\t test-send-error.py http://localhost:8000/ClientPost/JSON/ ./test-payload.json\n")
+
+    else:
+        send_data(sys.argv[1], sys.argv[2])
