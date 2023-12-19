6f7d06eee5a359b26efa89633a1b3cbeab19ca17
Rob Woolley <rob.woolley@windriver.com>
2023-12-19 04:52:15 -0800

    Move WSGI application out of project directory
    This is done to match layerindex-web and to keep the root directory
    consistent when running in productionwith Gunicorn.
    
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- settings.py
+++ settings.py
@@ -132,10 +132,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or
