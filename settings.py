cfe5cdefab6d1f270568ec794c7a9b793a070886
Rob Woolley <rob.woolley@windriver.com>
2023-12-19 05:13:34 -0800

    Add TEMPLATES to settings.py
    This resolves an error seen when running manage.py migrate:
    
    ERRORS:
    ?: (admin.E403) A 'django.template.backends.django.DjangoTemplates'
       instance must be configured in TEMPLATES in order to use the
       admin application.
    
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- settings.py
+++ settings.py
@@ -134,6 +134,28 @@ MIDDLEWARE = (
 
 ROOT_URLCONF = 'urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            TEMPLATES_PATH,
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
+
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wsgi.application'
 
