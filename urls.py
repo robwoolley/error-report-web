dcd9184966b68243584402c5ae3a60b3ad4ab9b6
Rob Woolley <rob.woolley@windriver.com>
2023-12-19 05:10:06 -0800

    Update Django urls functions
    Django has deprecated patterns and replaced url with re_path.  The
    view parameter of re_path also takes a callable function instead of
    url which took a string.
    
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@
 #
 # Licensed under the MIT license, see COPYING.MIT for details
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, re_path
 from django.contrib import admin
 from django.views.generic import TemplateView, RedirectView
 from django.conf import settings
@@ -14,32 +14,35 @@ from Post.views import results_mode
 from Post.feed import LatestEntriesFeed
 admin.autodiscover()
 
+from Post import views
+
 try:
     special_submitter = settings.SPECIAL_SUBMITTER['link']
 except AttributeError:
     special_submitter = "none"
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Uncomment the admin/doc line below to enable admin documentation:
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    #url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^(?i)Errors/Latest/$', 'Post.views.search', { 'mode' : results_mode.LATEST }, name= "latest_errors"),
-    url(r'^(?i)Errors/Latest/feed$', LatestEntriesFeed(), name="errors_feed"),
-    url(r'^(?i)Errors/Latest/'+special_submitter+'/$', 'Post.views.search', { 'mode' : results_mode.SPECIAL_SUBMITTER}, name= "latest_autobuilder_errors"),
-    url(r'^(?i)Errors/Latest/'+special_submitter+'/feed$', LatestEntriesFeed(results_mode.SPECIAL_SUBMITTER), name="special_submitter_errors_feed"),
-    url(r'^(?i)Errors/Search/$', 'Post.views.search', {'mode' : results_mode.SEARCH }, name = "errors_search"),
-    url(r'^(?i)Errors/Build/(?P<build_id>\d+)/$', 'Post.views.search', { 'mode' : results_mode.BUILD }, name= "build_errors"),
-    url(r'^(?i)Errors/Details/(?P<fail_id>\d+)/$', 'Post.views.details', name='details'),
-    url(r'^(?i)Errors/SimilarTo/(?P<fail_id>\d+)/$', 'Post.views.search', { 'mode' : results_mode.SIMILAR_TO }, name='similar'),
-    url(r'^(?i)Errors/Statistics/(?P<key>\w+)', 'Post.views.chart', {'template_name' : 'home.html'}, name= "statistics"),
-    url(r'^(?i)ClientPost/$', 'Post.views.addData'),
-    url(r'^(?i)ClientPost/JSON/$', 'Post.views.addData', { 'return_json' : True }),
-    url(r'^(?i)Errors/$', 'Post.views.default', name="main"),
-    url(r'^(?i)Statistics/$', TemplateView.as_view(template_name="home.html"), name = "statistics"),
-    url(r'^$', RedirectView.as_view(pattern_name="main", permanent=True)),
+    re_path(r'^admin/', admin.site.urls),
+    #re_path(r'^accounts/', include('registration.backends.default.urls')),
+    re_path(r'^Errors/Latest/$', views.search, { 'mode' : results_mode.LATEST }, name= "latest_errors"),
+    re_path(r'^Errors/Latest/feed$', LatestEntriesFeed(), name="errors_feed"),
+    re_path(r'^Errors/Latest/'+special_submitter+'/$', views.search, { 'mode' : results_mode.SPECIAL_SUBMITTER}, name= "latest_autobuilder_errors"),
+    re_path(r'^Errors/Latest/'+special_submitter+'/feed$', LatestEntriesFeed(results_mode.SPECIAL_SUBMITTER), name="special_submitter_errors_feed"),
+    re_path(r'^Errors/Search/$', views.search, {'mode' : results_mode.SEARCH }, name = "errors_search"),
+    re_path(r'^Errors/Build/(?P<build_id>\d+)/$', views.search, { 'mode' : results_mode.BUILD }, name= "build_errors"),
+    re_path(r'^Errors/Details/(?P<fail_id>\d+)/$', views.details, name='details'),
+    re_path(r'^Errors/SimilarTo/(?P<fail_id>\d+)/$', views.search, { 'mode' : results_mode.SIMILAR_TO }, name='similar'),
+    re_path(r'^Errors/Statistics/(?P<key>\w+)', views.chart, {'template_name' : 'home.html'}, name= "statistics"),
+    re_path(r'^ClientPost/$', views.addData),
+    re_path(r'^ClientPost/JSON$', views.addData, { 'return_json' : True }),
+    re_path(r'^ClientPost/JSON/$', views.addData, { 'return_json' : True }),
+    re_path(r'^Errors/$', views.default, name="main"),
+    re_path(r'^Statistics/$', TemplateView.as_view(template_name="home.html"), name = "statistics"),
+    re_path(r'^$', RedirectView.as_view(pattern_name="main", permanent=True)),
     # Url for backwards compatibility with old search links
-    url(r'^Errors/Search/Args/$', RedirectView.as_view(pattern_name="Post.views.search",query_string=True,permanent=True), {'mode':results_mode.SEARCH }),
-)
+    re_path(r'^Errors/Search/Args/$', RedirectView.as_view(pattern_name="views.search",query_string=True,permanent=True), {'mode':results_mode.SEARCH }),
+]
